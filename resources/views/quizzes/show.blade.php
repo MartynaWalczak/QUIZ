582deccd5b765cdfe15b09b7235d2464a4c09f40
Martyna Walczak <marti200307@gmail.com>
2025-11-18 18:24:15 +0100

    Initial commit

---
+++ resources/views/quizzes/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ $quiz['title'] }}</title>
+</head>
+<body style="background:#111; color:#fff; font-family:Arial; padding:30px;">
+    <h1>{{ $quiz['title'] }}</h1>
+
+    @foreach($quiz['questions'] as $index => $question)
+        <div style="margin-bottom:25px;">
+            <h3>{{ $question['question'] }}</h3>
+
+            @foreach($question['answers'] as $i => $answer)
+                <label style="display:block;">
+                    <input type="radio" name="q{{ $index }}" value="{{ $i }}">
+                    {{ $answer }}
+                </label>
+            @endforeach
+        </div>
+    @endforeach
+
+    <br>
+    <a href="/" style="color:orange;">← Powrót do listy quizów</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuizController;
+
+Route::get('/', [QuizController::class, 'index']);
+Route::get('/quiz/{id}', [QuizController::class, 'show']);
